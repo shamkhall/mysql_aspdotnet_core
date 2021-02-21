05821d501a33791677cdf3c4255cc64d1c78f400
Shamkhal <zeynalshamkhal@gmail.com>
2021-02-21 12:20:56 -0800

    image model, mysql, connectionstring

---
+++ MysqlAspnet/MysqlAspnet/Migrations/20210210214842_ImageMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class ImageMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    ImageName = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
---
+++ MysqlAspnet/MysqlAspnet/Models/ImageDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class ImageDbContext : DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext> options) : base(options)
+        { }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+
+}
